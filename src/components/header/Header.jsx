dedde589a67bec768cc0dc283c731d5d6331a118
BigAma <amareh.alibarreh@gmail.com>
2022-06-02 15:21:08 +0300

    Type-Writer effect added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,7 @@ import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
 
 import image from '../../assets/BigAma.png'
+import { Typewriter } from 'react-simple-typewriter'
 
 
 const Header = () => {
@@ -13,7 +14,17 @@ const Header = () => {
         <div id='top' className="container container__header">
             <h5>Hello, I'm</h5>
             <h1>Amareh Ali Barreh</h1>
-            <h5 className='text-light'>Fullstack Developper</h5>
+            <h5 className='text-light'>
+                <Typewriter 
+                    words={['Fullstack Developper', 'Designer', 'UI-UX Dev', 'Self-taught Programmer']}
+                    loop='true'
+                    cursor
+                    cursorStyle='|'
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                />
+            </h5>
             <CTA />
             <HeaderSocials />
             <div className='img-header__container'>
