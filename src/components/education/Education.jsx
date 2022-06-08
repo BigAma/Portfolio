e8622f72e9ae64c9f29e7ff75323c69541e001dc
Amareh <amareh.alibarreh@up-set.co>
2022-06-08 17:15:11 +0300

    Education components modified

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -6,6 +6,15 @@ const Education = () => {
   return (
     <div id='education' className='container container__education'>
         <h1>Education</h1>
+        <div className='resume-wrapper'>
+          <div className='resume-left-menu'>
+            <div className='resume-action-btn'><button>Resume</button></div>
+            <div className='resume-action-btn'><button>Skills</button></div>
+          </div>
+          <div className='resume-right-info'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta doloribus voluptatem aut exercitationem amet! Dolores cupiditate sequi dolorum sunt, alias error atque voluptas in sint. Asperiores </p>
+          </div>
+        </div>
     </div>
   )
 }
